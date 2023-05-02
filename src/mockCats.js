da623b14edadd55b305757cfe6e23258ef805a95
DeeMo44 <demario326@gmail.com>
2023-05-02 18:25:56 -0400

    frontend structure completed

---
+++ src/mockCats.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+  const mockCats = [
+    {
+      name: "Kitty 1",
+      age: 1,
+      enjoys: "break dancing",
+      image: "cat.com/url",
+      id: 1,
+    },
+    {
+      name: "Fat cat 2",
+      age: 2,
+      enjoys: "Cat nip and chilling",
+      image: "cat.com/url/1",
+      id: 2,
+    },
+    {
+      name: "Kitty cat cat 3",
+      age: 3,
+      enjoys: "Being a kitty cat cat",
+      image: "cat.com/url/2",
+      id: 3,
+    },
+    {
+      name: "Kitten Mitten 4",
+      age: 4,
+      enjoys: "Making biscuits",
+      image: "cat/com/url/3",
+      id: 4,
+    },
+  ];
+
+export default mockCats;

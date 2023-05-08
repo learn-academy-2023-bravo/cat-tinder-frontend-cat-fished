5986afd41285688fc6da61d57e322f13b1bfd4f6
MarkNou <mark.nou95@gmail.com>
2023-05-08 11:58:06 -0700

    styling added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <nav className="bg-gray-200 fixed bottom-0 w-full">
+    <nav className="bg-gray-800 fixed bottom-0 w-full">
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     
       <div className="flex justify-center">
-        <h1 className="font-bold text-xl ">Developed by Demario and Mark</h1>
+        <h1 className="font-bold text-xl text-white ">DeMarkio Productions ©</h1>
       </div>
       <div className="hidden md:block">
         <div className="ml-10 flex items-center space-x-4">
